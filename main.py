8ab68c4ce12942646bdd6a5c53428728987d20ba
Kangho_Noh <rkdgh98@khu.ac.kr>
2021-04-01 11:42:16 +0900

    scrapper 코드 가져와서 input에 맞는 직업군 검색기능

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
 
 app = Flask("SuperScrapper")
 
@@ -16,6 +17,8 @@ def report():
     word = request.args.get('word')
     if word:
         word = word.lower()
+        jobs = get_jobs(word)
+        print(jobs)
     else:
         return redirect("/")
     return render_template("report.html", searchingBy=word)
