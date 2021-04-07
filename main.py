cd857883ce4bcf6ec92676701a85d8fda9b5963d
Kangho_Noh <rkdgh98@khu.ac.kr>
2021-04-08 00:01:45 +0900

    스크래퍼와 html 결합, fake DB와 다운로드 기능 추가

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScrapper")
 
 # templates 폴더 안에 있는 html 문서 불러오기.
 # templates 폴더 안에 있으면 굳이 경로를 설정하지 않아도 flask가 알아서 찾는다.
+db = {}
 
 
 @app.route("/")
@@ -14,14 +16,38 @@ def home():
 
 @app.route("/report")
 def report():
-    word = request.args.get('word')
+    word = request.args.get('word')  # url에서 word라는 query argument 가져옴
     if word:
         word = word.lower()
-        jobs = get_jobs(word)
-        print(jobs)
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+
     else:
         return redirect("/")
-    return render_template("report.html", searchingBy=word)
+    return render_template("report.html",
+                           searchingBy=word,
+                           resultsNumber=len(jobs),
+                           jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect('/')
 
 
 app.run(host="127.0.0.1")
